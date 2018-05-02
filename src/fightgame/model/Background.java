f63ef1f6d8d7e49d4fbc04c51135d38a2c59aba6
romainmonnery <33548896+romainmonnery@users.noreply.github.com>
2018-05-02 08:39:55 +0100

    Images moved + modifications on background (loadong image in arraylist)and dimension (deleting useless staff)

--- src/fightgame/model/Background.java
+++ src/fightgame/model/Background.java
@@ -1,16 +1,33 @@
 package fightgame.model;
 
 import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 public class Background {
 	
-	public Background(Dimension dimension) {
-		
+	//attributes which specify the proportions in pixels of the background image
+	private int width = 336;
+	private int height = 800;
+	
+	public Background(int width, int height) {
+		this.width = width;
+		this.height = height;
 	}
 	
-	public Dimension getDimension() {
+	public void getImage() {
 		
+		ArrayList bImage = new ArrayList();
+		Image img;
+		
+		try {
+			img = ImageIO.read(new File("BackgroundImage.gif"));
+			bImage.add(img);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public Image getImage();
 }
--- src/fightgame/model/Dimension.java
+++ src/fightgame/model/Dimension.java
@@ -3,7 +3,7 @@ package fightgame.model;
 public class Dimension {
 
 	//Attributes which corresponds to the two caracteristics of a dimension : the width and the height
-	private int width;
+	/*private int width;
 	private int height;
 	
 	//Constructor which builds dimensions with the two attributes entered in parameters
@@ -26,5 +26,5 @@ public class Dimension {
 	//Getting the height
 	public int getHeight() {
 		return this.height;
-	}
+	}*/
 }
